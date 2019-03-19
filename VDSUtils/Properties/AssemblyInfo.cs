074de0472eac1e0802485644a664169616cbcf9a
koechlm <markus.koechl@autodesk.com>
2019-03-19 15:15:51 +0100

    updated for 2020 RTM

--- VDSUtils/Properties/AssemblyInfo.cs
+++ VDSUtils/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Quickstart VDS Utilities")]
-[assembly: AssemblyDescription("Quickstart VDS Extension 2019")]
+[assembly: AssemblyDescription("Quickstart VDS Extension 2020")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Autodesk")]
-[assembly: AssemblyProduct("Quickstart Utility Library 2019")]
+[assembly: AssemblyProduct("Quickstart Utility Library 2020")]
 [assembly: AssemblyCopyright("Copyright © Markus Koechl, Autodesk 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2019.24.0.0")]
-[assembly: AssemblyFileVersion("2019.24.0.0")]
+[assembly: AssemblyVersion("2020.25.0.0")]
+[assembly: AssemblyFileVersion("2020.25.0.0")]
